d897de538a235c3296ade48a20575028bd48dcd3
caiochaves01 <140531299+caiochaves01@users.noreply.github.com>
2023-08-19 15:36:11 -0300

    Enviado errado

--- _81_revisaoArrays/_81_revisaoArrays.js
+++
@@ -1,7 +0,0 @@
-// Valor por referência
-const nomes = ['Caio', 'Chaves', 'Silva'];
-// String, Objetos, Funções, Números
-// const nomes = new Array('Caio', 'Chaves', 'Silva');
-nomes[2] = 'Costa';
-delete nomes[2];
-console.log(nomes);
